2781859ab9d135ba40118aa5baf7a5344ba0ce2d
Danish <dan71ish@gmail.com>
2023-06-04 17:25:16 +0530

    uuid generated

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
+const { createServer } = require("http")
+const { Server } = require("socket.io");
+
+const uuid = require("uuid");
+
 const app = express();
+const httpServer = createServer();
+
+
+const io = new Server(httpServer, { /* options */ });
 app.set('view engine','ejs')
 app.use("/static", express.static('public'))
 
@@ -11,10 +21,11 @@ const port = 3000
 app.get('/', (req, res) => {
   res.render('pages/index');
 })
-app.post('/enter-room', (req, res) => {
-  console.log(req.body);
+app.get('/generate-room-id', (req, res) => {
+  // console.log(req.body);
+  let roomId = uuid.v4();
+  res.status(200).send({"roomId":roomId})
 })
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- public/js/home.js
+++ public/js/home.js
@@ -3,16 +3,17 @@ $(".submit-btn").click(function() {
         name: $('#name').val(),
         roomId: $("#room-id").val()
     }
-    if(data.name === ""|| data.roomId===""){
-        alert("Name and Room ID are required")
+    if(data.name === ""){
+        alert("Name is required")
     }else{
-        $.ajax({
-            "url":"/enter-room",
-            "type": "post",
-            "data": data, 
-            "success": function(resp){
-                console.log(resp)
-            }
-        })
+        if(data.roomId === ""){
+            $.ajax({
+                "url":"/generate-room-id",
+                "type": "get",
+                "success": function(resp){
+                    console.log(resp.roomId)
+                }
+            })
+        }
         }
 })
